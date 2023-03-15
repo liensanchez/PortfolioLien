43c3020cd025402ebe3f059f865c3ce831abc877
liensanchez <liensilviosanchez@gmail.com>
2023-03-15 02:54:09 -0300

    navBar for mobile, icons added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import NavBar from './Components/NavBar';
-import Presentation from './Components/Presentation';
+/* import Presentation from './Components/Presentation';
+import Projects from './Components/Projects';
+import Skills from './Components/Skills';
+import AboutMe from './Components/AboutMe';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer'; */
 
 function App() {
   return (
     <>
       <NavBar/>
-      <Presentation/>
-    </>
-
-    
+{/*       <Presentation/>
+      <Projects/>
+      <Skills/>
+      <AboutMe/>
+      <Contact/>
+      <Footer/> */}
+    </>    
   );
 }
 
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,14 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {HiMenu} from 'react-icons/hi'
+import {HiX} from 'react-icons/hi'
 import logo from '../assets/pyramid.png'
 
 function NavBar() {
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mobileQuery = window.matchMedia("(max-width: 1024px)");
+    setIsMobile(mobileQuery.matches);
+
+    const handleMobileCheck = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mobileQuery.addListener(handleMobileCheck);
+
+    return () => {
+      mobileQuery.removeListener(handleMobileCheck);
+    };
+  }, [])
+
+
+  const [menu, setMenu] = useState(false)
+
+  const OpenMenu = () => { 
+    setMenu(true)
+  }
+
+  const CloseMenu = () => {
+    setMenu(false)
+  }
+
+  
+
   return (
-    <div>
-      <img src={logo} alt="" />
-      <h3>Projects</h3>
-      <h3>Skills</h3>
-      <h3>About Me</h3>
-      <h3>Contact</h3>
+    <div className='flex flex-row justify-between m-8'>
+
+      <div >
+        <img src={logo} alt="" />
+      </div>
+
+      {isMobile ? 
+          <div className='flex flex-row p-1'>
+            {menu ? '' : <HiMenu onClick={OpenMenu}/>}
+            {menu ? <HiX onClick={CloseMenu}/> : ''}
+            <div className='flex flex-col bg-gray-50 bg-opacity-20 h-screen'>              
+              {menu && <h3 className='p-2'>Projects</h3>}
+              {menu && <h3 className='p-2'>Skills</h3>}
+              {menu && <h3 className='p-2'>About Me</h3>}
+              {menu && <h3 className='p-2'>Contact</h3> }
+            </div>
+          </div>
+    
+        :
+
+          <div className='flex flex-row'>
+            <h3 className='item'>Projects</h3>
+            <h3 className='item'>Skills</h3>
+            <h3 className='item'>About Me</h3>
+            <h3 className='item'>Contact</h3>
+          </div>
+      }
+
+
     </div>
   )
 }
