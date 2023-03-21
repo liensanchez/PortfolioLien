e3bfe3803d46bc8d894c9e56036a46f00bbfaa92
liensanchez <liensilviosanchez@gmail.com>
2023-03-21 11:47:28 -0300

    words fall inside my  head!

--- client/src/Components/Presentation.jsx
+++ client/src/Components/Presentation.jsx
@@ -6,9 +6,9 @@ function Presentation(isMobile) {
 
   let mobileCheck = isMobile.props
 
-/*   let start = window.innerWidth/2
+/*   let start = window.innerWidth/2*/
 
-  let half = window.innerWidth/2 */
+  let half = window.innerWidth/2 
 
   return (
     <>
@@ -23,14 +23,14 @@ function Presentation(isMobile) {
         <div className='flex flex-col items-center'>
         <motion.h2 
           initial={{x:-100}}
-          animate={{x:0,
+          animate={{x: 100,
                     rotate:90,
-                    y:100,
+                    y:500,
                     scale: 0}}
           transition={{ ease: "easeOut", duration: 5 }}
           className='z-0 -mt-10'
             >test</motion.h2>
-          <img src={Lien} alt="" className=' bg-red-300'/>
+          <img src={Lien} alt="" className='z-10 '/>
         </div>
       </div>
       </>
